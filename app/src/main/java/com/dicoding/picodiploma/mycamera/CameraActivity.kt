6c1e68d2422915be3f54937e2e6ab8967f9a5d90
Nurdin <mrsunnysummer@gmail.com>
2024-11-06 04:20:40 +0700

    feat: create barcode scanning functionality

--- app/src/main/java/com/dicoding/picodiploma/mycamera/CameraActivity.kt
+++ app/src/main/java/com/dicoding/picodiploma/mycamera/CameraActivity.kt
@@ -6,14 +6,23 @@ import android.util.Log
 import android.view.WindowInsets
 import android.view.WindowManager
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.mlkit.vision.MlKitAnalyzer
+import androidx.camera.view.CameraController.COORDINATE_SYSTEM_VIEW_REFERENCED
+import androidx.camera.view.LifecycleCameraController
 import androidx.core.content.ContextCompat
 import com.dicoding.picodiploma.mycamera.databinding.ActivityCameraBinding
+import com.google.mlkit.vision.barcode.BarcodeScanner
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.barcode.common.Barcode
 
 class CameraActivity : AppCompatActivity() {
+    private lateinit var  barcodeScanner: BarcodeScanner
     private lateinit var binding: ActivityCameraBinding
     private var cameraSelector: CameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
 
@@ -32,33 +41,41 @@ class CameraActivity : AppCompatActivity() {
     }
 
     private fun startCamera() {
-        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        val options = BarcodeScannerOptions.Builder()
+            .setBarcodeFormats(Barcode.FORMAT_QR_CODE)
+            .build()
 
-        cameraProviderFuture.addListener({
-            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
-            val preview = Preview.Builder()
-                .build()
-                .also {
-                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
-                }
+        barcodeScanner = BarcodeScanning.getClient(options)
 
-            try {
-                cameraProvider.unbindAll()
-                cameraProvider.bindToLifecycle(
-                    this,
-                    cameraSelector,
-                    preview
-                )
+        val analyzer = MlKitAnalyzer(
+            listOf(barcodeScanner),
+            COORDINATE_SYSTEM_VIEW_REFERENCED,
+            ContextCompat.getMainExecutor(this)
+        ) { result: MlKitAnalyzer.Result? ->
+            showResult(result)
+        }
 
-            } catch (exc: Exception) {
-                Toast.makeText(
-                    this@CameraActivity,
-                    "Gagal memunculkan kamera.",
-                    Toast.LENGTH_SHORT
-                ).show()
-                Log.e(TAG, "startCamera: ${exc.message}")
-            }
-        }, ContextCompat.getMainExecutor(this))
+        val cameraController = LifecycleCameraController(baseContext)
+        cameraController.setImageAnalysisAnalyzer(
+            ContextCompat.getMainExecutor(this),
+            analyzer
+        )
+
+        cameraController.bindToLifecycle(this)
+        binding.viewFinder.controller = cameraController
+    }
+
+    private fun showResult(result: MlKitAnalyzer.Result?) {
+        val barcodeResults = result?.getValue(barcodeScanner)
+        if((barcodeResults != null) && (barcodeResults.size != 0) && (barcodeResults.first() != null)) {
+            val barcode = barcodeResults[0]
+            val alertDialog = AlertDialog.Builder(this)
+                .setMessage(barcode.rawValue)
+                .setCancelable(false)
+                .create()
+
+            alertDialog.show()
+        }
     }
 
     private fun hideSystemUI() {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,5 +12,5 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
-rootProject.name = "MyCamera"
+rootProject.name = "MyBarcodeScanner"
 include(":app")
